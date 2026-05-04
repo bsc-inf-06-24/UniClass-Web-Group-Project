5778396cd29d09cf92b16df6f31467b3fe19aa2d
Patisto <spatisto@gmail.com>
2026-05-04 06:43:49 +0200

    import joshs module in main module

--- uniclass-backend/src/app.module.ts
+++ uniclass-backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { CoursesModule } from './courses/courses.module'; //josh
 
 @Module({
 imports: [
