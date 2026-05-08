cfbe194bec5f0b0ec4da41c37fdb8ab59f06fd15
Josh-L23 <joshualobeni@gmail.com>
2026-05-08 15:44:54 +0200

    Add swagger

--- uniclass-backend/src/main.ts
+++ uniclass-backend/src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+
 async function bootstrap() {
-const app = await NestFactory.create(AppModule);
-app.enableCors();
-app.useGlobalPipes(new ValidationPipe());
-await app.listen(3000);
-console.log('UniClass running on http://localhost:3000');
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('UniClass API')
+    .setDescription('The UniClass API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+  console.log('UniClass running on http://localhost:3000');
+  console.log('Swagger docs available at http://localhost:3000/api/docs');
 }
 bootstrap();
